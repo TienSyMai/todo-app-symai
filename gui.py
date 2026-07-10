4be1cf844d60a75bffab70d0a0bcae13521844e5
Sy Mai <mrroboteagle.nexus@gmail.com>
2026-07-10 13:02:40 +0700

    complete_exit_button

--- gui.py
+++ gui.py
@@ -7,9 +7,13 @@ input_box = sg.InputText(tooltip="Enter todo box", key="todo")
 add_button = sg.Button("Add your holly to do")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events= True, size=[45, 10])
 edit_button = sg.Button("Edit")
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label], 
+                   [input_box, add_button], 
+                   [list_box, edit_button, complete_button],
+                   [exit_button]],
                    font=("Helvetica", 15))
 while True: 
     event, values = window.read()
@@ -33,8 +37,17 @@ while True:
             functions.write_todos(todos)
 
             window["todos"].update(values=todos)
+        case "Complete":
+             todo_to_complete = values["todos"][0]
+             todos = functions.get_todos()
+             todos.remove(todo_to_complete)
+             functions.write_todos(todos)
+             window["todos"].update(values=todos)
+             window["todo"].update(value="")
         case "todos":
-            window['todo'].update(value=values["todos"][0])
+            window['todo'].update(value=values["todos"][0].strip('\n'))
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
     print('hao')
